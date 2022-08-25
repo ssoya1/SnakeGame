8bd9197f6cac9ed0eb80f222a9ed5bda484a53f1
ssoya1 <08sophia.olefir15@gmail.com>
2022-08-25 15:20:44 +0300

    add board function

--- Program.cs
+++ Program.cs
@@ -2,11 +2,43 @@
 
 namespace snake
 {
-    internal class Program
+    internal class Snake
     {
+        int Height = 20;
+        int Width = 30;
+
+        public void WriteBoard()
+        {
+            Console.Clear();
+            for(int i = 1;i <=(Width+2); i++)
+            {
+                Console.SetCursorPosition(i, 1);
+                Console.Write("-");
+
+            }
+            for (int i = 1; i <= (Width + 2); i++)
+            {
+                Console.SetCursorPosition(i, (Height+2));
+                Console.Write("-");
+
+            }
+            for (int i = 1; i <= (Height + 1); i++)
+            {
+                Console.SetCursorPosition(1, i);
+                Console.Write("|");
+            }
+            for (int i = 1; i <= (Height + 1); i++)
+            {
+                Console.SetCursorPosition((Width + 2), i);
+                Console.Write("|");
+
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Snake snake = new Snake();
+            snake.WriteBoard();
+            Console.ReadKey();
         }
     }
 }
